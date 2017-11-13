6237dde5d4d0d166cd1a4af7379ffa40ac836aca
mioyui88 <mioyui88@gmail.com>
2017-11-13 09:38:10 +0800

    connect mysql

--- hello.py
+++ hello.py
@@ -1,45 +1,39 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_script import Manager
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from flask import Flask
+from flask import request
+from flaskext.mysql import MySQL
+import json
+
+mysql = MySQL()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+ 
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'cnvd'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
-manager = Manager(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+@app.route("/")
+def hello():
+    return "Welcome to Python Flask App!"
 
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+@app.route("/ip")
+def ip():
+    ip = request.args.get('IP')
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from DEVICE_list_0 where DEV_ip = '"+ip+"' ")
+    data = cursor.fetchall()
+    if data == ():
+     return "no data"
+    else:
+     r = json.dumps(data)
+     return r
 
 
-if __name__ == '__main__':
-    manager.run()
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, % s!</h1>' % name
+
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
